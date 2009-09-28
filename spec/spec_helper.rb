9c1cc29fbfceb61c8850bf27042d91ab8fa50c32
Justin Blake <justin@megablaix.com>
2009-09-28 13:15:03 -0400

    Using fakeweb to block all real web requests from specs in case something slips through without a mock.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,10 @@ require 'spec'
 require 'spec/autorun'
 require 'fakeweb'
 
+# Let's keep these specs self-contained.
+# All web service requests should be mocked.
+FakeWeb.allow_net_connect = false
+
 Spec::Runner.configure do |config|
   
 end
